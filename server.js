ca0dd761e21e22c85cbbd66abc8e7800ccd44b3a
Michael Hu <upsidedownhu@gmail.com>
2020-02-22 05:21:21 -0800

    push to heroku

--- server.js
+++ server.js
@@ -1,181 +1,42 @@
+// Web Scraper Homework Solution Example
+// (be sure to watch the video to see
+// how to operate the site in the browser)
+// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+
+// Require our dependencies
 var express = require("express");
-var logger = require("morgan");
 var mongoose = require("mongoose");
-var path = require("path");
+var exphbs = require("express-handlebars");
 
-// Our scraping tools
-var axios = require("axios");
-var cheerio = require("cheerio");
+// Set up our port to be either the host's designated port, or 3000
+var PORT = process.env.PORT || 3000;
 
-// Require all models
-var db = require("./models");
-
-var PORT = process.env.port;
-
-// Initialize Express
+// Instantiate our Express App
 var app = express();
 
-// Configure middleware
+// Require our routes
+var routes = require("./routes");
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-//use handlebars for front-end
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({
-    defaultLayout: "main",
-    partialsDir: path.join(__dirname, "/views/layouts/partials")
-}));
+// Connect Handlebars to our Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Have every request go through our route middleware
+app.use(routes);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Connect to the Mongo DB
-//mongoose.connect("mongodb://localhost/ArticleDb", {useNewUrlParse : true });
-//var PORT = process.env.PORT || 3000;
-
-//mongoose.connect(MONGODB_URI);
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoartnews";
-
 mongoose.connect(MONGODB_URI);
 
-app.get("/scraped", function(req, res) {
-    axios.get("http://www.echojs.com/").then(function(response) {
-      var $ = cheerio.load(response.data);
-      //console.log("scraped response:" + JSON.stringify(response.data, null, '\t')); 
-      $("article h2").each(function(i, element) {
-        
-        var result = {};
-
-        result.title = $(this)
-        .children("a")
-        .text();
-
-        result.link = $(this)
-        .children("a")
-        .attr("href");
-
-        result.summary = $(this)
-        .children("a")
-        .text();
-
-        
-        //console.log("the result");
-        console.log("scraped result:" + JSON.stringify(result, null, '\t'));
-        db.Article.create(result)
-        .then(function(dbArticle) {
-            console.log(dbArticle);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
-      });
-
-});
-res.send("Scrape Complete");
-});
-
-//console.log(JSON.stringify(db.Article));
-//Show all the article that is not saved
-
-app.get("/",function(req, res){
-    //console.log("here is the code");
-    console.log('"here is the code" ' + JSON.stringify(db.Article));
-    db.Article.find({"saved": false}).then(function(dbArticle){
-        var hbsObject ={articles: dbArticle};
-        res.render("index",hbsObject);
-    }).catch(function(err){
-        res.json(err);
-    });
-});
-
-//get all the saved article to  from db
-app.get("/saved", function(req, res) {
-    // Grab every document in the Article collection
-    db.Article.find({"saved": true})
-        .populate("notes")
-        .then(function(dbArticle) {
-            console.log("from database"+dbArticle)
-            var hbsObject= {articles: dbArticle}
-            // If we were able to successfully find Article, send them back to the client
-            res.render("saved",hbsObject);
-        })
-        .catch(function(err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
-});
-//post when client needs to save
-app.post("/saved/:id", function(req,res){
-    db.Article.findOneAndUpdate({"_id": req.params.id},{"$set": {"saved":true}})
-    .then(function(dbArticle){
-        res.json(dbArticle+"situatioin changes");
-    }).catch(function(err){
-        res.json(err);
-    });
-});
-
-app.post("/delete/:id",function(req,res){
-    db.Article.findByIdAndDelete({"_id": req.params.id},{"set": {"saved":false}})
-    .then(function(result){
-        res.json(result);
-    }).catch(function(err){
-        res.json(err);
-    });
-});
-
-//get one specific article from db
-app.get("/articles/:id",function(req,res){
-    db.Article.findOne({"_id": req.params.id })
-    .populate("notes")
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    }).catch(function(err) { res.json(err); });
-
-});
-
-//creat note for article
-app.post("/article/:id",function(req,res){
-    db.Note.create(req.boby).then(function(dbNote){
-        return db.Article.findOneAndUpdate({"_id": req.params.id }, {"notes": dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle){
-        res.json(dbArticle);
-    }).catch(function(err){
-        res.json(err);
-    });
-});
-
-// delete note
-app.post("/deleteNote/:id", function(req, res){
-    db.Note.remove({"_id": req.params.id})
-      .then(function(dbArticle){
-        res.json(dbArticle);
-      })
-      .catch(function(err) { 
-        res.json(err) 
-      });
-});
-
-//delete all the note and the colletion
- app.get("/clearall", function(req, res) {
-     db.Article.remove({})
-     .then(function(result) {
-         res.json(result);
-       })
-       .catch(function(err) {
-         res.json(err);
-       });
- })
-
-
-// Start the server
+// Listen on the port
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
-  
+  console.log("Listening on port: " + PORT);
+});
